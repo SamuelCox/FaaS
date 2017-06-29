2e18efa465d323ba177168af4b6efe2fff96e362
samuelcox <sc630@kent.ac.uk>
2017-06-30 00:59:38 +0100

    Same as last commit just forgot to add some files.

---
+++ FaaS/FaaS/Models/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaaS.Models
+{
+    public class Settings
+    {        
+
+        public  List<string> Connections { get; set; }
+
+    }
+}
---
+++ FaaS/FaaS/Scripts/Functions.js
@@ -0,0 +1,23 @@
+﻿function addConnection(connection) {
+    XMLHttpRequest xhr = new XMLHttpRequest();
+    var cookie = getCookie('user');
+    var url = 'addconnection?userName=' + cookie + '&connection='connection;
+    xhr.open("POST", "AddConnection");
+    xhr.send();
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
